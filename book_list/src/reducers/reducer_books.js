7f4c02ab20aea1f97c78e42b9771cf36e725efff
RoniRamon <roniramon260@gmail.com>
2018-06-14 14:39:29 -0700

    section 4:39 Reducers

---
+++ book_list/src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+export default function() {
+  return [
+    {title: 'Cracking the Coding Interview: '}, // Gayle Laakmann McDowell
+    {title: 'Algorithms (4th Edition)'}, // Robert Sedgewick and Kevin Wayne
+    {title: 'Python for Kids: A Playful Introduction To Programming'}, //Jason R. Briggs
+    {title: 'JavaScript and jQuery: Interactive Front-End Web Development Hardcover'}, // Jon Duckett'
+    {title: 'SQL All-in-One For Dummies' }, // Allen G. Taylor'
+    {title: 'Programming the Raspberry Pi, Second Edition: Getting Started with PythonOct'}, //Simon Monk
+    {title: 'Learning PHP, MySQL & JavaScript: With jQuery, CSS & HTML5'}, //Robin Nixon
+  ];
+}
