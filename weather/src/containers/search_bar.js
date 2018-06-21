56bee92d52bd8aa721088d1fc763b52785377f6e
RoniRamon <roniramon260@gmail.com>
2018-06-21 14:20:17 -0700

    redux promises and axios to get api data

--- weather/src/containers/search_bar.js
+++ weather/src/containers/search_bar.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {term: ''};
+  }
+
+  onInputChange(e) {
+    this.setState({term: e.target.value});
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    //get weather data when form submit !!!
+    
+  }
 
   render() {
     return (
         <div>
-          <form className="input-group">
-            <input />
+          <form className="input-group" onSubmit={this.onFormSubmit}>
+            <input
+              onChange={this.onInputChange.bind(this)}
+              value={this.state.term}
+              className="form-control"
+              placeholder="Get a five-day forecast in your favorite cities"
+            />
             <span className="input-group-btn">
               <button type="submit" className="btn btn-secondary">Submit</button>
             </span>
